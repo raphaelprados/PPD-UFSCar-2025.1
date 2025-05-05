d2cf7a1a3cb14e4c996697f00d46f3836519a223
raphaelprados <santosapraphael@gmail.com>
2025-05-05 14:36:21 -0300

    Must fix grid value overwriting

--- laplace/pthreads/laplace_pthreads.cpp
+++ laplace/pthreads/laplace_pthreads.cpp
@@ -17,6 +17,7 @@
 #include<sys/time.h>
 
 #include<iostream>
+#include<fstream>
 #include<vector>
 #include<string>
 
@@ -47,12 +48,12 @@ typedef struct thread_data{
 // ------------------------------ Global variables ------------------------------
 
 int size;
-int iterations;
+int iterations = -1;
 int global_chunk_size;
 
 double *grid;
 double *new_grid;
-double total_error = 1.0e-1f;
+double total_error = 1.0;
 
 pthread_mutex_t chunk_mutex;
 pthread_cond_t chunks_available,
@@ -98,6 +99,7 @@ void allocate_memory(){
 }
 
 void deallocate_memory(){
+    printf("Dealocating\n");
     free(grid);
     free(new_grid);
 }
@@ -114,7 +116,7 @@ void save_grid(){
 
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            fprintf(file, "%lf ", grid[size*i + j]);
+            fprintf(file, "%lf;", grid[size*i + j]);
         }
         fprintf(file, "\n");
     }
@@ -127,7 +129,7 @@ void print_grid(int size) {
 
     for(i = 0; i < size; i++) {
         for(j = 0; j < size; j++) {
-            printf("%lf", grid[size*i + j]);
+            printf("| %.1lf |", grid[size*i + j]);
         }
         printf("\n");
     }
@@ -148,7 +150,7 @@ int set_first_pos(chunk_info &ck) {
 int get_n_pos(chunk_info ck, int n) {
     int stencil_pos_int, stencil_pos, stencil_row, stencil_col, first_pos;
 
-    stencil_pos_int = (ck.size*ck.id);
+    stencil_pos_int = (ck.size*ck.id+n);
     stencil_pos = size + 1 + stencil_pos_int/(size-2)*2 + stencil_pos_int;
 
     return stencil_pos;
@@ -203,16 +205,18 @@ chunk_info* create_chunks(int n, int size) {
 void* producer(void *arg) {
     
     thread_data *ptd = (thread_data*)arg;
+    double max_error;
     int computed_chunks;
+    int i, j;
 
     pthread_barrier_wait(&barrier);
 
-    while(CONV_THRESHOLD < total_error) {
+    while(CONV_THRESHOLD < total_error && iterations < ITER_MAX) {
 
         computed_chunks = 0;    
         iterations++;
 
-        printf("Producer Started\n");
+        // printf("Producer Started\n");
 
         pthread_mutex_lock(&chunk_mutex);
 
@@ -221,26 +225,45 @@ void* producer(void *arg) {
                 pthread_cond_wait(&chunks_unavailable, &chunk_mutex);
             }
             
-            // Resetando chunk para próxima iteração
+            // print_grid(size);
+            // getchar();
+            
+            total_error = 1.1e-5f;
+            // Obtendo erro total e resetando chunk para próxima iteração
             for(int i = 0; i < ptd->n; i++) {
-                ptd->chunks[i].status = 'u';
                 total_error = max(total_error, ptd->chunks[i].local_err);
-                ptd->chunks[i].local_err = 1.0e-1f;
+
+                // printf("Chunk %d computed with error %.6lf agains %.6lf\n", i, ptd->chunks[i].local_err, total_error);
+
+                ptd->chunks[i].status = 'u';
+                ptd->chunks[i].local_err = 1.1e-5f;
             }
+
+            // save_grid();
             
             reset_chunks = false;
-            iterations++;
+            // iterations++;
 
-            for(int i = 0; i < ptd->n; i++) {
-                printChunk(ptd->chunks[i]);
-                chunk_traverse(ptd->chunks[i]);
+            // Array swapping for the next iteration
+            if(iterations > 0) {
+                double *temp;
+                temp = grid;
+                grid = new_grid;
             }
+
+            printf("Iteration %d finished with %.16lf error\n", iterations, total_error);
+            print_grid(size);
+            getchar();
+
+            // getchar();
 
             pthread_cond_signal(&chunks_available);
         pthread_mutex_unlock(&chunk_mutex);
 
-        printf("Producer Finished\n");
+        // printf("Producer Finished\n");
     }
+
+    printf("Producer finished\n");
 
     return NULL;    
 }
@@ -258,19 +281,29 @@ double compute_stencil(chunk_info thread_chunk) {
         stencil_row = stencil_pos / size;
         stencil_col = stencil_pos % size;
 
-        // result[i] = 0.25 * (grid[ijTo1D(stencil_row - 1, stencil_col)] +
-        //                     grid[ijTo1D(stencil_row + 1, stencil_col)] + 
-        //                     grid[ijTo1D(stencil_row, stencil_col - 1)] + 
-        //                     grid[ijTo1D(stencil_row, stencil_col + 1)]);
+        result[i] = 0.25 * (grid[ijTo1D(stencil_row - 1, stencil_col)] +
+                            grid[ijTo1D(stencil_row + 1, stencil_col)] + 
+                            grid[ijTo1D(stencil_row, stencil_col - 1)] + 
+                            grid[ijTo1D(stencil_row, stencil_col + 1)]);
 
-        // err = max(err, absolute(result[i] - grid[stencil_pos]));
+        err = max(err, absolute(result[i] - grid[stencil_pos]));
+
+        printf("(%d, %d) Result[%d][%d] = 0.25 * ([%d][%d] (%.2lf), [%d][%d] (%.2lf), [%d][%d] (%.2lf), [%d][%d] (%.2lf)) = %lf (err: %lf)\n",
+                iterations, thread_chunk.id, stencil_col, stencil_row, 
+                stencil_row - 1, stencil_col, grid[ijTo1D(stencil_row - 1, stencil_col)],
+                stencil_row + 1, stencil_col, grid[ijTo1D(stencil_row + 1, stencil_col)],
+                stencil_row, stencil_col - 1, grid[ijTo1D(stencil_row, stencil_col - 1)],
+                stencil_row, stencil_col + 1, grid[ijTo1D(stencil_row, stencil_col + 1)],
+                result[i], err);
     }
 
+    // getchar();
+
     // Escreve os resultados da computação no grid
-    // for(int i = 0; i < thread_chunk.size; i++) {
-    //     stencil_pos = get_n_pos(thread_chunk, i);
-    //     new_grid[stencil_pos] = result[stencil_pos]; 
-    // }
+    for(int i = 0; i < thread_chunk.size; i++) {
+        stencil_pos = get_n_pos(thread_chunk, i);
+        new_grid[stencil_pos] = result[i]; 
+    }
 
     return err;
 }
@@ -288,28 +321,34 @@ void* consumer(void * arg) {
 
     pthread_barrier_wait(&barrier);
 
-    while(CONV_THRESHOLD < total_error) {
+    printf("Consumer %d started\n", t_data->id);
+
+    while(CONV_THRESHOLD < total_error && iterations < ITER_MAX) {
         
         done_chunks = 0;
         compute = true;
 
         if(!reset_chunks) {
             pthread_mutex_lock(&chunk_mutex);
+                done_chunks = 0;
                 for(int i = 0; i < t_data->n; i++) {
                     // Selects an available chunk
                     // printf("---<<<%d>>>---\n", (t_data->chunks[i].status == 'u'));
                     if(t_data->chunks[i].status == 'u') {
                         t_chunk = &t_data->chunks[i];
                         t_chunk->status = 'p';
-                        printf("Thread %d selected %dth chunk for computing\n", t_data->id, t_chunk->id);
+                        // printf("Thread %d selected %dth chunk for computing\n", t_data->id, t_chunk->id);
                         // temp = "Thread " + std::to_string(t_data->id) + " selected " + std::to_string(t_chunk->id) + "th chunk for computing"; 
                         // buffer.push_back(temp);
                         break;
+                    } else {
+                        done_chunks++;
                     }
                     // Checks if all chunks were already processed. If so, signals to the
                     //      producer. 
                     if(done_chunks == t_data->n) {
                         pthread_cond_signal(&chunks_unavailable);
+                        // printf("Signal Condition Full\n");
                         reset_chunks = true;
                         compute = false;
                     }
@@ -318,12 +357,15 @@ void* consumer(void * arg) {
 
             // Calcula o chunk e anota o erro
             if(compute) {
-                printf("Thread %d computed the %dth chunk\n", t_data->id, t_chunk->id);
-                getchar();
+                // printf("Thread %d computed the %dth chunk\n", t_data->id, t_chunk->id);
+                // getchar();
                 t_chunk->local_err = compute_stencil(*t_chunk);
             }
         }   
     }
+
+    printf("Consumer %d finished\n", t_data->id);
+
     return NULL;
 }
 
@@ -354,6 +396,9 @@ int main(int argc, char const *argv[]) {
     pthread_t consumer_Threads[n_threads];
     pthread_t producer_Thread;    
 
+    // Matrix Iterator Variables 
+    int i, j;
+
     // Thread information variable (shared between all threads)
     thread_data *thread_info = new thread_data[n_threads+1]; 
     thread_info[0] = {chunks, n_chunks, 0};
@@ -379,30 +424,28 @@ int main(int argc, char const *argv[]) {
     pthread_cond_init(&chunks_available, NULL);
     pthread_cond_init(&chunks_unavailable, NULL);
 
-
     // -------------------------------- Processing Area --------------------------------
 
 
     printf("Laplace execution starting with grid[%d][%d] and %d threads!\n", size, size, n_threads);
 
-    pthread_barrier_init(&barrier, NULL, n_threads + 1);
+    pthread_barrier_init(&barrier, NULL, n_threads + 2);
 
     pthread_create(&producer_Thread, NULL, producer, (void*)&(thread_info[0]));
 
     for(int i = 1; i <= n_threads; i++) {
+        printf("Thread %d created\n", thread_info[i].id);
         pthread_create(&consumer_Threads[i], nullptr, &consumer, (void*)&(thread_info[i]));
     }
 
     pthread_barrier_wait(&barrier);
+
+    pthread_join(producer_Thread, NULL);
+
+    for(int i = 0; i <= n_threads; i++) 
+        pthread_join(producer_Thread, NULL);
+
     pthread_barrier_destroy(&barrier);
-
-    for(int i = 0; i <= n_threads; i++) {
-        if(i == 0)
-            pthread_join(producer_Thread, NULL);
-        else
-            pthread_join(consumer_Threads[i], NULL);
-    }
-
 
     // -------------------------------- Post-Processing Area --------------------------------
 
@@ -410,6 +453,8 @@ int main(int argc, char const *argv[]) {
 
     // Deallocate the data 
     deallocate_memory();
+
+    printf("Finished deallocation");
 
     return 0;
 }
